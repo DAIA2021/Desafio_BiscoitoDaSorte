aa845e1ee158df2883c9b1d6722c3c0405fc0caa
Daiana Oliveira <daiana.davi2411@gmail.com>
2023-03-07 14:43:45 -0300

    altrerações de código

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 
 
-const btn = document.querySelector('#btn')
+const btnClick = document.querySelector('#btnClick')
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
+const buttonReset = document.querySelector('#buttonReset')
 
 const phrases = document.querySelector('.phrasesOfCookie')
-
-const button = document.querySelector('#button')
 
 const phrasesOfCookie = [
   'Se alguém está tão cansado que não possa te dar um sorriso deixe-lhe o teu',
@@ -14,14 +13,21 @@ const phrasesOfCookie = [
 ]
 
 
-btn.addEventListener('click',btnClick)
+
+const toggleScreen = () => {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+  phrases.innerText = phrasesOfCookie[Math.round(Math.random() * 2)];
+};
+
+btnClick.addEventListener("click", toggleScreen);
+buttonReset.addEventListener("click", toggleScreen);
 
 
-function btnClick() {
-  document.querySelector(".screen1").classList.add("hide");
-  document.querySelector(".screen2").classList.remove("hide");
-  phrases.innerText = phrasesOfCookie[Math.round(Math.random() * 2)]
-}
+
+
+
+
 
 
 
